e72442f2fa4d1d3b86db5c7680ce3434823eff44
Artem <artem.yaremenko.1992@gmail.com>
2025-05-28 20:43:02 +0300

    WellDone

--- src/main/java/pro/sky/Main.java
+++ src/main/java/pro/sky/Main.java
@@ -3,7 +3,7 @@ package pro.sky;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Напишите пример обработки нескольких исключений в одном блоке catch.");
 
         try {
 //            String s = null;
